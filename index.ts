7a0bdbf9c518ff2b61ef2a4cc2b19ad0dafc6d1b
Per-Emil Johansson <chimikoo@gmail.com>
2024-03-13 14:17:39 +0100

    blabla

--- index.ts
+++ index.ts
@@ -1,17 +1,22 @@
 import express from 'express';
-import { type Response } from 'express';
+import type { Request, Response } from 'express';
 
 const app = express();
 const port = 8080;
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 interface HelloResponse {
   message: string; 
 }
 
-app.get("/", (req, res: Response<HelloResponse>) => {
+app.get("/", (req: Request, res: Response<HelloResponse>) => {
   res.json({ message: "Hello from Bun & Express, using Docker!" });
 });
 
+
 app.listen(port, () => {
-  console.log(`Listening on port ${port}...`);
+  if (process.env.NODE_ENV !== 'production') {
+    console.log(`Listening on port ${port}...`);
+  }
 });
