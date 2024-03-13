65d66165c47be44b5b557620cb5c53d2a8e40403
Per-Emil Johansson <chimikoo@gmail.com>
2024-03-13 13:18:37 +0100

    ts update

--- index.ts
+++ index.ts
@@ -1,13 +1,14 @@
 import express from 'express';
+import { type Response } from 'express';
 
 const app = express();
 const port = 8080;
 
 interface HelloResponse {
-  message: string;
+  message: string; 
 }
 
-app.get("/", (req, res: express.Response<HelloResponse>) => {
+app.get("/", (req, res: Response<HelloResponse>) => {
   res.json({ message: "Hello from Bun & Express, using Docker!" });
 });
 
