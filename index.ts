bd0d18e6c8f8fd860231b46aeb018be1a51fda67
Per-Emil Johansson <chimikoo@gmail.com>
2024-03-13 12:22:40 +0100

    text update

--- index.ts
+++ index.ts
@@ -4,7 +4,7 @@ const app = express();
 const port = 8080;
 
 app.get("/", (req, res) => {
-    res.json({ message: "Hello from Bun & Express!" });
+    res.json({ message: "Hello from Bun & Express, using Docker!" });
   });
 
 app.listen(port, () => {
