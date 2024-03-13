4d52211d97e7cdb226e1dd5107ccd89386f336c4
Per-Emil Johansson <chimikoo@gmail.com>
2024-03-13 12:47:37 +0100

    added ts so marco is happy

--- index.ts
+++ index.ts
@@ -1,11 +1,15 @@
-import express from "express";
+import express from 'express';
 
 const app = express();
 const port = 8080;
 
-app.get("/", (req, res) => {
-    res.json({ message: "Hello from Bun & Express, using Docker!" });
-  });
+interface HelloResponse {
+  message: string;
+}
+
+app.get("/", (req, res: express.Response<HelloResponse>) => {
+  res.json({ message: "Hello from Bun & Express, using Docker!" });
+});
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}...`);
